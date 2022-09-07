d23a985a2c92080bb6762b062d7d7e63168bdde3
opv1 <pas7273@gmail.com>
2022-09-07 21:34:53 +0300

    commit

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,8 @@
+import '../styles/styles.scss';
+
 import { PAGES } from 'src/constants';
 import AuthController from 'src/controllers/AuthController';
 import router from 'src/router';
-
-import '../styles/styles.scss';
 
 document.addEventListener('DOMContentLoaded', async () => {
   PAGES.forEach((page) => {
